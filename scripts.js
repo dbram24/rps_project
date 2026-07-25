9f6276e28e3f8a00affc5bbbd8f3ccd09d8263fa
David <dbram24@gmail.com>
2026-07-25 18:57:38 -0400

    Add if statement for each win/lose condition
    Declares the winner of the round and increments the winner's score.

--- scripts.js
+++ scripts.js
@@ -53,10 +53,36 @@ function playRound(humanChoice,computerChoice) {
     humanChoice = humanChoice.toLowerCase();
     //console.log(humanChoice); - testing lowercase method on humanChoice
 
-
+    if (humanChoice == computerChoice) {
+        console.log("Tie! Try again");
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore += 1;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock!");
+        humanScore += 1;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats Paper!");
+        humanScore += 1;
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock!");
+        computerScore += 1;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats Paper!");
+        computerScore += 1;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore += 1;
+    } else {
+        console.log("Please input a valid response!");
+    }
 }
-
 const humanSelection = getHumanChoice();
-const computerSelection =getComputerChoice();
+const computerSelection = getComputerChoice();
+//const computerSelection = "rock"; - testing each case
 
 playRound(humanSelection,computerSelection);
+console.log("Human Score:");
+console.log(humanScore);
+console.log("Computer Score:");
+console.log(computerScore);

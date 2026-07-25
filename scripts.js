5aaf2a8e2112ee0801af87b7e9782101e68a5192
David <dbram24@gmail.com>
2026-07-25 17:31:13 -0400

    Adds external javascript file
    Keeps html file cleaner and more readable.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/* When getComputerChoice runs
+Generate a random number
+Based on that random number, return "Rock", "Paper", or "Scissors"
+*/
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+}
+
+getComputerChoice();
